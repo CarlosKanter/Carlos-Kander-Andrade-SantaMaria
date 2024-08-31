69278260dfc15f688a10edc94c7a73d0e9002f55
CarlosKanter <personal.laboral.academico@gmail.com>
2024-08-31 10:11:47 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const showMessageBtn = document.getElementById('showMessageBtn');
+  showMessageBtn.addEventListener('click', function() {
+      alert('¡Hola! Este es un mensaje de JavaScript.');
+  });
+});
